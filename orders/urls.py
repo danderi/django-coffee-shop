86d0cae2f7b207be1a06b7ccc23f53b524896599
danderi <anderi.daniel@gmail.com>
2026-06-14 18:11:19 -0400

    Add order creation, product to order feature and order detail template

--- orders/urls.py
+++ orders/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from .views import MyOrderView, CreateOrderProductView
+
 urlpatterns = [
-    
+    path("my-order/", MyOrderView.as_view(),  name = "my_order"),
+    path("add-product", CreateOrderProductView.as_view(), name = "add_product")
 ]
